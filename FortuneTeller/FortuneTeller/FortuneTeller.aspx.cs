d48430833f5626d15686beb6978abb5e380fc9fd
Anthony Johnson <anthony.johnson@structuredforgrowth.com>
2018-01-08 19:03:56 -0800

    FortuneTeller
    Made the second challenge!

---
+++ FortuneTeller/FortuneTeller/FortuneTeller.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FortuneTeller
+{
+    public partial class FortuneTeller : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void clickMe_Click(object sender, EventArgs e)
+        {
+            string ageValue = ageTextBox.Text;
+            string moneyValue = moneyTextBox.Text;
+
+            string result = "At " + ageValue + " years old, I would have expected you to have more than " + moneyValue + " in your pocket.";
+
+            resultLabel.Text = result;
+        }
+    }
+}
